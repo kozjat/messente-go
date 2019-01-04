28f2d4759cc72ef997112fe8260a42052b2d6f67
kozjat <arvo.saalits@gmail.com>
2019-01-04 15:56:05 +0200

    Fix typo

--- sms/errors.go
+++ sms/errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 // ErrorCodes variable
 var ErrorCodes = map[string]error{
-	"ERROR 101":  errors.New("[SMS API] Access is restricted, wring credentials"),
+	"ERROR 101":  errors.New("[SMS API] Access is restricted, wrong credentials"),
 	"ERROR 102":  errors.New("[SMS API] Parameters are wrong (or missing)"),
 	"ERROR 103":  errors.New("[SMS API] Invalid IP address - not in the whitelist"),
 	"ERROR 111":  errors.New("[SMS API] Sender parameter \"from\" is invalid"),
